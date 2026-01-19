8a796f6bfbbe62007c9493cbe8bac862b4e8c07b
Steinbrinker <ls68bino@uni-leipzig.de>
2026-01-19 17:35:18 +0100

    update content summer term 2026

--- docs/js/week-control.js
+++ docs/js/week-control.js
@@ -1,5 +1,5 @@
-const es_week = 1;
-const css_week =1;
+const es_week = 0;
+const css_week =0;
 
 document.addEventListener("DOMContentLoaded", () => {
   const allLinks = document.querySelectorAll('nav a');
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Found link:", href);
 
-    // Check for experimental sociology links
-    if (href.includes("experimental-sociology/week")) {
-      const match = href.match(/week(\d{2})/i);
+    // Check for Experimental Sociology links in "Course Assessment"
+    if (href.includes("course assessment/exercise-sheet")) {
+      const match = href.match(/exercise-sheet-(\d)/i);
       if (match) {
-        const weekNum = parseInt(match[1], 10);
-        if (weekNum > es_week) {
-          console.log(`Hiding ES link for week ${weekNum}`);
+        const sheetNum = parseInt(match[1], 10);
+        if (sheetNum > es_sheet) {
+          console.log(`Hiding ES link for exercise sheet ${sheetNum}`);
           link.closest("li")?.style.setProperty("display", "none", "important");
         }
       }
